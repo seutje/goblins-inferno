f21d7745d3e30eccca3bb58ec854b7c441e9bae8
Steve De Jonghe <seutje@gmail.com>
2025-09-10 09:21:47 +0200

    Persist audio mute preference (#12)

--- tests/audio.test.js
+++ tests/audio.test.js
@@ -1,8 +1,25 @@
-import { playSound, initAudio, setMuted } from '../src/js/audio.js';
+import { playSound, initAudio, setMuted, loadMuted } from '../src/js/audio.js';
 
-test('audio functions exist and are callable in Node', () => {
-  // In Node/Jest, initAudio should no-op and playSound should not throw
-  expect(() => initAudio()).not.toThrow();
-  expect(() => setMuted(true)).not.toThrow();
-  expect(() => playSound('fire')).not.toThrow();
+describe('audio helpers', () => {
+  beforeEach(() => {
+    const store = {};
+    global.localStorage = {
+      getItem: (k) => (k in store ? store[k] : null),
+      setItem: (k, v) => { store[k] = v; }
+    };
+  });
+
+  test('audio functions exist and are callable in Node', () => {
+    // In Node/Jest, initAudio should no-op and playSound should not throw
+    expect(() => initAudio()).not.toThrow();
+    expect(() => setMuted(true)).not.toThrow();
+    expect(() => playSound('fire')).not.toThrow();
+  });
+
+  test('setMuted persists preference', () => {
+    setMuted(true);
+    expect(loadMuted()).toBe(true);
+    setMuted(false);
+    expect(loadMuted()).toBe(false);
+  });
 });
